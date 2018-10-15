82a9667efdf5c72c2fc235c659fefdaf38c3b51c
Blast06 <lacalculadora06@hotmail.com>
2018-10-14 22:08:45 -0400

    algunas notas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,6 +18,34 @@ export class HomePage {
 
   getAlb()
   {
+
+    /*
+
+    LEE ESTO:
+
+    en la linea 31 cuando llamo al metodo que esta en el provider, utilizando la libreria httpclient.
+    fijate bien como accedo al objeto despues del subscribe pongo "data:any" porque asi no me dara erro el 
+    editor al utilizar acciones como en la linea 35 "data.data", aunque tambien se puede acceder a partes de un
+    objeto de la manera que vez en la linea 36, que incluso puede ser mejor en muchos casos.
+    En las otras lineas fui entrando mas al objeto hasta llegar donde decias. Despues lo imprimo en el console.log
+    para que veas como queda. Y despues lo hago con un foreach recorriendolos.
+
+
+    DOS cosas:
+
+    en la vista, lo haras de otra forma, no con un foreach, lo haras con un NgFor(de este lado no se usa ngfor)
+    
+    la otra es....
+
+    que se pudo haber hecho algo como esto:
+
+    this.tracks =data.data.0.tracks.data;
+
+    pero bueeno, se ve raro no?
+
+    */
+
+
     this.alb.getDataTracksOne().subscribe((data:any) => {
       console.log(data.data);
       this.tracks = data.data['0'];
