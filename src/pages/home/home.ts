ea50ff21641ce1fce65146dd986a88758ef33ccb
Blast06 <lacalculadora06@hotmail.com>
2018-10-14 21:59:33 -0400

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { AlbumsProvider } from './../../providers/albums/albums';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -7,8 +8,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  tracks:any[];
+  constructor(public navCtrl: NavController,
+              public alb:AlbumsProvider) {
+
+                this.getAlb();
 
   }
+
+  getAlb()
+  {
+    this.alb.getDataTracksOne().subscribe((data:any) => {
+      console.log(data.data);
+      this.tracks = data.data['0'];
+      this.tracks = this.tracks['tracks'];
+      this.tracks = this.tracks['data'];
+
+      console.log(this.tracks);
+
+      this.tracks.forEach((track) => {
+        console.log(track.title_short);
+      })
+
+    })
+  }
+
+
+
+  
 
 }
